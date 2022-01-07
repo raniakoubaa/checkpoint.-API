5c7c0667e99645e94bff60ed2e0049b4177e3353
rania koubaa <koubaarania33@gmail.com>
2022-01-07 14:05:58 +0100

    chk.api

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import UserList from './UserList/UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> List of User </h1>
+      <UserList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UserList/UserList.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import { Table } from 'react-bootstrap';
+
+const UserList = () => {
+    const [listOfUSer, setListOfUSer] = useState([])
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    // .then(function (response) {
+    //          return response.json();
+    //          //console.log(response)
+    //      })
+    //      .then(function (data) {
+    //              setListOfUSer(data);
+    //          })
+    //          .catch(function (err) {
+    //                  console.log('error: ' + err);
+    //              });
+
+     useEffect(() => {
+        function fetchData() {
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(function (response) {
+                    return response.json();
+                    //console.log(response)
+                })
+                .then(function (data) {
+                    setListOfUSer(data);
+                })
+                .catch(function (err) {
+                    console.log('error: ' + err);
+                                });
+                }
+                   fetchData();
+        }, []);
+    return (
+        <div>
+    <Table striped bordered hover width="50%">
+  <thead>
+    <tr>
+    <th> Name</th>
+    <th> User Name</th>
+    <th> Email </th>
+     <th> Phone Number </th>
+     <th>website</th>
+     <th> company</th>
+    </tr>
+  </thead>
+  <tbody>   
+    {listOfUSer.map(item => (
+        <tr key={item.id}> 
+                        <td>{item.name}</td>
+                         <td>{item.username}</td>
+                        <td>{item.email}</td>
+                        <td>{item.phone}</td>
+                        <td>{item.website}</td>
+                        <td>{item.company.name}</td> 
+                        
+                        </tr>
+                ))}
+  </tbody>
+</Table>
+        </div>
+    )
+}
+
+export default UserList
